51ef7df4e290cca474d33106effd7612126547b2
Evert Ingwersen <everti2000@gmail.com>
2025-03-26 15:04:03 +0100

    transfer model to other laptop

--- AIS_NN_draft.py
+++ AIS_NN_draft.py
@@ -43,7 +43,7 @@ print("Modules imported!")
 #------------------ INPUT PARAMETERS ------------------------------------------
 
 learning_type = 'sklearn'
-random_seed = 13122
+random_seed = 5684
 
 # Wich data to train? 'JSON', 'CSV', or 'Both'
 data_type = 'CSV'
@@ -684,7 +684,7 @@ match learning_type:
         print("")
 
         # Initialize the model
-        train_nn = MLPClassifier(hidden_layer_sizes=(6,12,24,50,50,24,12,6),
+        train_nn = MLPClassifier(hidden_layer_sizes=(6,12,24,400,400,24,12,6),
                                  solver='adam',
                                  max_iter=2000,  
                                  warm_start=True,  # Keeps the previous model state to continue from last fit
