1987a40d8a9d6f467a2071a21c1a11870db70025
Evert Ingwersen <everti2000@gmail.com>
2025-03-26 14:07:41 +0100

    change random seed

--- AIS_NN_draft.py
+++ AIS_NN_draft.py
@@ -42,7 +42,7 @@ print("Modules imported!")
 #------------------ INPUT PARAMETERS ------------------------------------------
 
 learning_type = 'sklearn'
-random_seed = 5321
+random_seed = 7736
 
 # Wich data to train? 'JSON', 'CSV', or 'Both'
 data_type = 'CSV'
